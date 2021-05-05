bb5f148e0bf89c2dcc9735ca2677f5063436930f
GayaGorodecki <58183247+GayaGorodecki@users.noreply.github.com>
2021-05-05 14:50:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/haircutscheduling/activities/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.haircutscheduling.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.haircutscheduling.R;
+import com.example.haircutscheduling.fragments.LoginFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+//        LoginFragment loginFragment = new LoginFragment();
+//
+//        fragmentTransaction.add(R.id.fragmentcon, loginFragment).commit();
+
+    }
+}
